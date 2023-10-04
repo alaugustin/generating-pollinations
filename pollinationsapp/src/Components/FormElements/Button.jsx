65ad8907ccf837bbe93b1a24b273a82854e458ca
Al Augustin <al@alaugustin.com>
2023-10-04 02:20:27 +0000

    logging form data

--- pollinationsapp/src/Components/FormElements/Button.jsx
+++ pollinationsapp/src/Components/FormElements/Button.jsx
@@ -1,6 +1,7 @@
 export default function Button(props) {
   const { buttonLabel } = props;
+
   return (
-    <button type="button" className="basis-full" id="submitPrompt" onClick={() => console.log("Hello!")}>{buttonLabel}</button>
+    <button type="submit" className="basis-full" id="submitPrompt">{buttonLabel}</button>
   )
 }
--- pollinationsapp/src/Components/FormElements/Form.jsx
+++ pollinationsapp/src/Components/FormElements/Form.jsx
@@ -6,8 +6,20 @@ export default function Form(props) {
   const { descriptionInputType, descriptionId, descriptionValueText, descriptionLabelText, sourceUrlInputType, sourceUrlId, sourceUrlValueText, sourceUrlLabelText, visualStyleIdid, visualStyleLabelText, visualStyleOptions, artistReferenceInputType, artistReferenceId, artistReferenceLabelText, descriptionLabelClasses, sourceUrlLabelClasses, visualStyleLabelClasses, artistReferenceLabelClasses, submitButtonLabel } = props;
   const commonFieldsetClasses = 'flex pb-8';
 
+  const handleSubmit = (event) => {
+    const description = event.target[1].value;
+    const sourceUrl = event.target[2].value;
+    const visualStyle = event.target[4].value;
+    const artistReference = event.target[5].value;
+    const resultsArray = [description, sourceUrl, visualStyle, artistReference];
+
+    event.preventDefault();
+
+    console.log(resultsArray);
+  }
+
   return (
-    <form className="flex flex-col flex-wrap">
+    <form className="flex flex-col flex-wrap" onSubmit={handleSubmit}>
       <fieldset className={commonFieldsetClasses}>
         <LabelAndInput inputType={descriptionInputType} id={descriptionId} valueText={descriptionValueText} labelText={descriptionLabelText} customLabelClassName={descriptionLabelClasses}/>
 
